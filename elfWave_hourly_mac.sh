b5f9883a2fbbe2546db01a2000a7d27da12eba48
Michael Guse <mguse@salesforce.com>
2018-03-19 11:56:54 -0500

    Further ipdates to hourly shell script

--- elfWave_hourly_mac.sh
+++ elfWave_hourly_mac.sh
@@ -35,224 +35,237 @@
  #* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  #*/
 
-#prompt the user to enter their username for the target (Wave) org
-read -p "Please enter the Wave target org username (and press ENTER): " tUsername
-
-#prompt the user to enter their password for the target (Wave) org
-read -s -p "Please enter the Wave target org password (and press ENTER): " tPassword
-
-#prompt the user to enter their username for the source (Event Monitoring) org
-read -p "Please enter username for Event Monitoring source org (and press ENTER): " username
-
-#prompt the user to enter their password for the source (Event Monitoring) org
-read -s -p "Please enter password for Event Monitoring source org (and press ENTER): " password
-
-#prompt the user to enter their instance end-point for the source (Event Monitoring) org
-echo 
-read -p "Please enter instance (e.g. na1) for the for Event Monitoring source org loginURL (and press ENTER): " instance
+# Uncomment the environment config file that you are using
+source prod.conf
+#source load.conf
 
 #prompt the user to enter the date for the logs they want to download for the source (Event Monitoring) org
 read -p "Please enter logdate (e.g. Yesterday, Last_Week, Last_n_Days:5) (and press ENTER): " day
+echo $day
 
 #set API version to the proper level to the supported EventTypes listed below
 api_version='v42.0'
 
+#eventType=''
+#eventInterval=''
+
 #prompt the user to enter the eventType they want to download for the source (Event Monitoring) org
 printf 'What EventType do you want to download?\n'
-#/** printf '1. All 45 event types (Default)\n'
-   printf '2. API\n'
-#*   printf '3. ApexCallout\n'
-#*   printf '4. ApexExecution\n'
-#*   printf '5. ApexSoap\n'
-#*   printf '6. ApexTrigger\n'
-#*   printf '7. AsyncReportRun\n'
-   printf '8. BulkApi\n'
-#*   printf '9. ChangeSetOperation\n'
-#*   printf '10. Console\n'
-#*   printf '11. ContentDistribution\n'
-#*   printf '12. ContentDocumentLink\n'
-#*   printf '13. ContentTransfer\n'
-#*   printf '14. Dashboard\n'
-#*   printf '15. DocumentAttachmentDownloads\n'
-#*   printf '16. ExternalCrossOrgCallout\n'
-#*   printf '17. ExternalCustomApexCallout\n'
-#*   printf '18. ExternalOdataCallout\n'
-#*   printf '19. InsecureExternalAssets\n'
-#*   printf '20. KnowledgeArticleView\n'
-#*   printf '21. LightningError\n'
-#*   printf '22. LightningInteraction\n'
-   printf '23. LightningPageView\n'
-#*   printf '24. LightningPerformance\n'
-#*   printf '25. Login\n'
-#*   printf '26. LoginAs\n'
-#*   printf '27. Logout\n'
-#*   printf '28. MetadataApiOperation\n'
-#*   printf '29. MultiBlockReport\n'
-#*   printf '30. PackageInstall\n'
-#*   printf '31. PlatformEncryption\n'
-#*   printf '32. QueuedExecution\n'
-#*   printf '33. Report\n'
-#*   printf '34. ReportExport\n'
-   printf '35. RestApi\n'
-#*   printf '36. Sandbox\n'
-#*   printf '37. Search\n'
-#*   printf '38. SearchClick\n'
-#*   printf '39. Sites\n'
-#*   printf '40. TimeBasedWorkflow\n'
-#*   printf '41. TransactionSecurity\n'
-#*   printf '42. URI\n'
-#*   printf '43. VisualforceRequest\n'
-#*   printf '44. WaveChange\n'
-#*   printf '45. WaveInteraction\n'
-#*   printf '46. WavePerformance\n'
+printf '1. All 45 event types\n'
+printf '2. API\n'
+printf '3. ApexCallout\n'
+printf '4. ApexExecution\n'
+printf '5. ApexSoap\n'
+printf '6. ApexTrigger\n'
+printf '7. AsyncReportRun\n'
+printf '8. BulkApi\n'
+printf '9. ChangeSetOperation\n'
+printf '10. Console\n'
+printf '11. ContentDistribution\n'
+printf '12. ContentDocumentLink\n'
+printf '13. ContentTransfer\n'
+printf '14. Dashboard\n'
+printf '15. DocumentAttachmentDownloads\n'
+printf '16. ExternalCrossOrgCallout\n'
+printf '17. ExternalCustomApexCallout\n'
+printf '18. ExternalOdataCallout\n'
+printf '19. InsecureExternalAssets\n'
+printf '20. KnowledgeArticleView\n'
+printf '21. LightningError\n'
+printf '22. LightningInteraction\n'
+printf '23. LightningPageView\n'
+printf '24. LightningPerformance\n'
+printf '25. Login\n'
+printf '26. LoginAs\n'
+printf '27. Logout\n'
+printf '28. MetadataApiOperation\n'
+printf '29. MultiBlockReport\n'
+printf '30. PackageInstall\n'
+printf '31. PlatformEncryption\n'
+printf '32. QueuedExecution\n'
+printf '33. Report\n'
+printf '34. ReportExport\n'
+printf '35. RestApi\n'
+printf '36. Sandbox\n'
+printf '37. Search\n'
+printf '38. SearchClick\n'
+printf '39. Sites\n'
+printf '40. TimeBasedWorkflow\n'
+printf '41. TransactionSecurity\n'
+printf '42. URI\n'
+printf '43. VisualforceRequest\n'
+printf '44. WaveChange\n'
+printf '45. WaveInteraction\n'
+printf '46. WavePerformance\n'
 
 read eventMenu
 
 case $eventMenu in 
-#     1)
-#          eventType=${eventType:-All}
-#          ;;
+     1)
+          eventType=${eventType:-All}
+          ;;
      2)
           eventType=${eventType:-API}
           ;;
-#     3)
-#          eventType=${eventType:-ApexCallout}
-#          ;; 
-#     4)
-#          eventType=${eventType:-ApexExecution}
-#          ;; 
-#     5)
-#          eventType=${eventType:-ApexSoap}
-#          ;;      
-#     6)
-#          eventType=${eventType:-ApexTrigger}
-#          ;; 
-#     7)
-#          eventType=${eventType:-AsyncReportRun}
-#          ;; 
+     3)
+          eventType=${eventType:-ApexCallout}
+          ;; 
+     4)
+          eventType=${eventType:-ApexExecution}
+          ;; 
+     5)
+          eventType=${eventType:-ApexSoap}
+          ;;      
+     6)
+          eventType=${eventType:-ApexTrigger}
+          ;; 
+     7)
+          eventType=${eventType:-AsyncReportRun}
+          ;; 
      8)
           eventType=${eventType:-BulkApi}
           ;; 
-#     9)
-#          eventType=${eventType:-ChangeSetOperation}
-#          ;; 
-#     10)
-#          eventType=${eventType:-Console}
-#          ;;
-#     11)
-#          eventType=${eventType:-ContentDistribution}
-#          ;; 
-#     12)
-#          eventType=${eventType:-ContentDocumentLink}
-#          ;; 
-#     13)
-#          eventType=${eventType:-ContentTransfer}
-#          ;; 
-#     14)
-#          eventType=${eventType:-Dashboard}
-#          ;; 
-#     15)
-#          eventType=${eventType:-DocumentAttachmentDownloads}
-#          ;; 
-#     16)
-#          eventType=${eventType:-ExternalCrossOrgCallout}
-#          ;;
-#     17)
-#          eventType=${eventType:-ExternalCustomApexCallout}
-#          ;;
-#     18)
-#          eventType=${eventType:-ExternalOdataCallout}
-#          ;;
-#     19)
-#          eventType=${eventType:-InsecureExternalAssets}
-#          ;;
-#     20)
-#          eventType=${eventType:-KnowledgeArticleView}
-#          ;;
-#     21)
-#          eventType=${eventType:-LightningError}
-#          ;;
-#     22)
-#          eventType=${eventType:-LightningInteraction}
-#          ;;
-     23)
-          eventType=${eventType:-LightningPageView}
+     9)
+          eventType=${eventType:-ChangeSetOperation}
+          ;; 
+     10)
+          eventType=${eventType:-Console}
           ;;
-#     24)
-#          eventType=${eventType:-LightningPerformance}
-#          ;;          
-#     25)
-#          eventType=${eventType:-Login}
-#          ;; 
-#     26)
-#          eventType=${eventType:-LoginAs}
-#          ;; 
-#     27)
-#          eventType=${eventType:-Logout}
-#          ;; 
-#     28)
-#          eventType=${eventType:-MetadataApiOperation}
-#          ;; 
-#     29)
-#          eventType=${eventType:-MultiBlockReport}
-#          ;; 
-#     30)
-#          eventType=${eventType:-PackageInstall}
-#          ;; 
-#     31)
-#          eventType=${eventType:-PlatformEncryption}
-#          ;;
-#     32)
-#          eventType=${eventType:-QueuedExecution}
-#          ;;
-#     33)
-#          eventType=${eventType:-Report}
-#          ;; 
-#     34)
-#          eventType=${eventType:-ReportExport}
-#          ;; 
+     11)
+          eventType=${eventType:-ContentDistribution}
+          ;; 
+     12)
+          eventType=${eventType:-ContentDocumentLink}
+          ;; 
+     13)
+          eventType=${eventType:-ContentTransfer}
+          ;; 
+     14)
+          eventType=${eventType:-Dashboard}
+          ;; 
+     15)
+          eventType=${eventType:-DocumentAttachmentDownloads}
+          ;; 
+     16)
+          eventType=${eventType:-ExternalCrossOrgCallout}
+          ;;
+     17)
+          eventType=${eventType:-ExternalCustomApexCallout}
+          ;;
+     18)
+          eventType=${eventType:-ExternalOdataCallout}
+          ;;
+     19)
+          eventType=${eventType:-InsecureExternalAssets}
+          ;;
+     20)
+          eventType=${eventType:-KnowledgeArticleView}
+          ;;
+     21)
+          eventType=${eventType:-LightningError}
+          ;;
+     22)
+          eventType=${eventType:-LightningInteraction}
+          ;;
+      23)
+           eventType=${eventType:-LightningPageView}
+           ;;
+     24)
+          eventType=${eventType:-LightningPerformance}
+          ;;          
+     25)
+          eventType=${eventType:-Login}
+          ;; 
+     26)
+          eventType=${eventType:-LoginAs}
+          ;; 
+     27)
+          eventType=${eventType:-Logout}
+          ;; 
+     28)
+          eventType=${eventType:-MetadataApiOperation}
+          ;; 
+     29)
+          eventType=${eventType:-MultiBlockReport}
+          ;; 
+     30)
+          eventType=${eventType:-PackageInstall}
+          ;; 
+     31)
+          eventType=${eventType:-PlatformEncryption}
+          ;;
+     32)
+          eventType=${eventType:-QueuedExecution}
+          ;;
+     33)
+          eventType=${eventType:-Report}
+          ;; 
+     34)
+          eventType=${eventType:-ReportExport}
+          ;; 
      35)
           eventType=${eventType:-RestApi}
           ;; 
-#     36)
-#          eventType=${eventType:-Sandbox}
-#          ;; 
-#     37)
-#          eventType=${eventType:-Search}
-#          ;;
-#     38)
-#          eventType=${eventType:-SearchClick}
-#          ;;
-#     39)
-#          eventType=${eventType:-Sites}
-#          ;; 
-#     40)
-#          eventType=${eventType:-TimeBasedWorkflow}
-#          ;; 
-#     41)
-#          eventType=${eventType:-TransactionSecurity}
-#          ;;
-#     42)
-#          eventType=${eventType:-URI}
-#          ;; 
-#     43)
-#          eventType=${eventType:-VisualforceRequest}
-#          ;; 
-#     44)
-#          eventType=${eventType:-WaveChange}
-#          ;;
-#     45)
-#          eventType=${eventType:-WaveInteraction}
-#          ;;
-#     46)
-#          eventType=${eventType:-WavePerformance}
-#          ;;
-#     *)
-#          eventType=${eventType:-All}
-#          ;;
+     36)
+          eventType=${eventType:-Sandbox}
+          ;; 
+     37)
+          eventType=${eventType:-Search}
+          ;;
+     38)
+          eventType=${eventType:-SearchClick}
+          ;;
+     39)
+          eventType=${eventType:-Sites}
+          ;; 
+     40)
+          eventType=${eventType:-TimeBasedWorkflow}
+          ;; 
+     41)
+          eventType=${eventType:-TransactionSecurity}
+          ;;
+     42)
+          eventType=${eventType:-URI}
+          ;; 
+     43)
+          eventType=${eventType:-VisualforceRequest}
+          ;; 
+     44)  
+          eventType=${eventType:-WaveChange} 
+          ;;
+     45)  
+          eventType=${eventType:-WaveInteraction} 
+          ;;
+     46)  
+          eventType=${eventType:-WavePerformance} 
+          ;;
+      *)  
+          echo "$eventMenu is not a valid option"
+          ;;
 esac
 
 echo ${eventType}
+
+#prompt the user to choose daily or hourly files they want to download for the source (Event Monitoring) org
+printf 'What EventInterval do you choose?\n'
+printf '1. Daily\n'
+printf '2. Hourly\n'
+
+read intervalChoice
+
+case $intervalChoice in 
+     1)
+          eventInterval=${eventInterval:-Daily}
+          ;;
+     2)
+          eventInterval=${eventInterval:-Hourly}
+          ;;
+     *)  
+          echo "$intervalChoice is not a valid option"
+          ;;
+esac
+
+echo ${eventInterval}
 
 #set access_token for OAuth flow 
 #change client_id and client_secret to your own connected app - bit.ly/sfdcConnApp
@@ -261,37 +274,53 @@ access_token=`curl https://${instance}.salesforce.com/services/oauth2/token -d "
 #uncomment next line if you want to check your access token
 #echo "Access token: ${access_token}"
 
-if [ $eventType == All ]; then
-
-    #set elfs to the result of ELF query *without* EventType in query
-    elfs=`curl https://${instance}.salesforce.com/services/data/${api_version}/query?q=Select+Id+,+EventType+,+LogDate+From+EventLogFile+Where+Sequence+!=+0+AND+LogDate+=+${day} -H "Authorization: Bearer ${access_token}" -H "X-PrettyPrint:1"`
-
-else
-    #set elfs to the result of ELF query *with* EventType in query
-    elfs=`curl https://${instance}.salesforce.com/services/data/${api_version}/query?q=Select+Id+,+EventType+,+LogDate,+Sequence+From+EventLogFile+Where+Sequence+!=+0+AND+LogDate+=+${day}+AND+EventType+=+\'${eventType}\' -H "Authorization: Bearer ${access_token}" -H "X-PrettyPrint:1"`
+if [ $eventInterval == "Hourly" ]; then
+    if [ $eventType == "All" ]; then
+        #set elfs to the result of ELF query *without* EventType in query
+        elfs=`curl https://${instance}.salesforce.com/services/data/${api_version}/query?q=Select+Id+,+EventType+,+LogDate,+Sequence+From+EventLogFile+Where+Sequence+!=+0+AND+LogDate+%3E=+${day} -H "Authorization: Bearer ${access_token}" -H "X-PrettyPrint:1"`
+    else
+        #set elfs to the result of ELF query *with* EventType in query
+        elfs=`curl https://${instance}.salesforce.com/services/data/${api_version}/query?q=Select+Id+,+EventType+,+LogDate,+Sequence+From+EventLogFile+Where+Sequence+!=+0+AND+LogDate+%3E=+${day}+AND+EventType+=+\'${eventType}\' -H "Authorization: Bearer ${access_token}" -H "X-PrettyPrint:1"`
+    fi
 fi
-# Uncomment next line to debug response object from EventLogFIle query
-# echo ${elfs}
+
+if [ $eventInterval == "Daily" ]; then
+    if [ $eventType == "All" ]; then
+        #set elfs to the result of ELF query *without* EventType in query
+        elfs=`curl https://${instance}.salesforce.com/services/data/${api_version}/query?q=Select+Id+,+EventType+,+LogDate,+Sequence+From+EventLogFile+Where+LogDate+%3E=+${day} -H "Authorization: Bearer ${access_token}" -H "X-PrettyPrint:1"`
+    else
+        #set elfs to the result of ELF query *with* EventType in query
+        elfs=`curl https://${instance}.salesforce.com/services/data/${api_version}/query?q=Select+Id+,+EventType+,+LogDate,+Sequence+From+EventLogFile+Where+LogDate+%3E=+${day}+AND+EventType+=+\'${eventType}\' -H "Authorization: Bearer ${access_token}" -H "X-PrettyPrint:1"`
+    fi
+fi 
+
+# Uncomment next line to debug response object from EventLogFile query
+echo ${elfs}
 
 #set the three variables to the array of Ids, EventTypes, and LogDates which will be used when downloading the files into your local directory
 ids=( $(echo ${elfs} | jq -r ".records[].Id") )
-eventTypes=( $(echo ${elfs} | jq -r ".records[].EventType" ) )
+logDates=( $(echo ${elfs} | jq -r ".records[].LogDate" | cut -c 1-10 ) )
+logHours=( $(echo ${elfs} | jq -r ".records[].LogDate" | cut -c 12-13 ) )
 sequences=( $(echo ${elfs} | jq -r ".records[].Sequence" ) )
-logDates=( $(echo ${elfs} | jq -r ".records[].LogDate" | cut -c 1-13 ) )
+eventTypes=( $(echo ${elfs} | jq -r ".records[].EventType" ) )
+
+#make directory to store the files by date and separate out raw data from 
+#converted timezone data
+mkdir "eventlogs/${eventInterval}-raw"
+mkdir "eventlogs/${eventInterval}"
 
 #loop through the array of results and download each file with the following naming convention: EventType.csv
 for i in "${!ids[@]}"; do
     
-    #make directory to store the files by date and separate out raw data from 
-    #converted timezone data
-    mkdir "${eventTypes[$i]}-raw"
-    mkdir "${eventTypes[$i]}"
-
     #download files into the ${eventTypes[$i]}-raw directory
-    curl --compressed "https://${instance}.salesforce.com/services/data/${api_version}/sobjects/EventLogFile/${ids[$i]}/LogFile" -H "Authorization: Bearer ${access_token}" -H "X-PrettyPrint:1" -o "${eventTypes[$i]}-raw/${eventTypes[$i]}-${logDates[$i]}-${sequences[$i]}.csv" 
+    curl --compressed \
+        "https://${instance}.salesforce.com/services/data/${api_version}/sobjects/EventLogFile/${ids[$i]}/LogFile" \
+        -H "Authorization: Bearer ${access_token}" \
+        -H "X-PrettyPrint:1" \
+        -o "eventlogs/${eventInterval}-raw/${logDates[$i]}_${logHours[$i]}-${sequences[$i]}_${eventTypes[$i]}.csv" 
 
-    #convert files into the ${eventTypes[$i]} directory for Salesforce Analytics
-    awk -F ','  '{ if(NR==1) printf("%s\n",$0); else{ for(i=1;i<=NF;i++) { if(i>1&& i<=NF) printf("%s",","); if(i == 2) printf "\"%s-%s-%sT%s:%s:%sZ\"", substr($2,2,4),substr($2,6,2),substr($2,8,2),substr($2,10,2),substr($2,12,2),substr($2,14,2); else printf ("%s",$i);  if(i==NF) printf("\n")}}}' "${eventTypes[$i]}-raw/${eventTypes[$i]}-${logDates[$i]}-${sequences[$i]}.csv" > "${eventTypes[$i]}/${eventTypes[$i]}-${logDates[$i]}-${sequences[$i]}.csv"
+    #convert files into the ${eventInterval} directory for Salesforce Analytics
+    awk -F ','  '{ if(NR==1) printf("%s\n",$0); else{ for(i=1;i<=NF;i++) { if(i>1&& i<=NF) printf("%s",","); if(i == 2) printf "\"%s-%s-%sT%s:%s:%sZ\"", substr($2,2,4),substr($2,6,2),substr($2,8,2),substr($2,10,2),substr($2,12,2),substr($2,14,2); else printf ("%s",$i);  if(i==NF) printf("\n")}}}' "eventlogs/${eventInterval}-raw/${logDates[$i]}_${logHours[$i]}-${sequences[$i]}_${eventTypes[$i]}.csv" > "eventlogs/${eventInterval}/${logDates[$i]}_${logHours[$i]}-${sequences[$i]}_${eventTypes[$i]}.csv"
 
 done
 
@@ -341,8 +370,8 @@ if [ $del == Y ] || [ $del == y ] || [ $del == Yes ] || [ $del == yes ]; then
         rm "${uEventTypes[$i]}.csv"
     done
     rm -r "archive"
-    #leave data and directories for audit reasons
     echo "The files were removed."
+    #leave data and directories for audit reasons
 elif [ $del == N ] || [ $del == n ] || [ $del == No ] || [ $del == no ]; then
     echo "The files will remain."
 fi
