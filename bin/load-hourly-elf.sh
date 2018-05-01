8ed032f91ab772733e4193ba40ff1d9e86d2f3a2
Michael Guse <mguse@salesforce.com>
2018-05-01 17:21:42 -0400

    Added security token to passwords in conf file

--- bin/load-hourly-elf.sh
+++ bin/load-hourly-elf.sh
@@ -138,13 +138,8 @@ printf 'Event log start TS:\t\t %s\n' ${day}
 #set access_token for OAuth flow 
 #change client_id and client_secret to your own connected app - bit.ly/sfdcConnApp
 
-echo ===
-echo === [$(date "+%Y-%m-%d %H:%M:%S")] - Retrieve access token from Salesforce sandbox environment 
-echo ===
-access_token=`curl -v https://login.salesforce.com/services/Soap/u/v${api_version} -H "Content-Type: text/xml; charset=UTF-8" -H "SOAPAction: login" -d @$HOME/bin/sfdcprod.login | jq -r '.sessionId'`
-
-#login=`curl -v https://${instance}.salesforce.com/services/oauth2/token -d "grant_type=password" -d "client_id=3MVG99OxTyEMCQ3ilfR5dFvVjgTrCbM3xX8HCLLS4GN72CCY6q86tRzvtjzY.0.p5UIoXHN1R4Go3SjVPs0mx" -d "client_secret=7899378653052916471" -d "username=${username}" -d "password=${password}" -H "X-PrettyPrint:1"`
-#access_token=( $(echo ${login} | jq -r '.access_token') )
+login=`curl -v https://${instance}.salesforce.com/services/oauth2/token -d "grant_type=password" -d "client_id=3MVG99OxTyEMCQ3ilfR5dFvVjgTrCbM3xX8HCLLS4GN72CCY6q86tRzvtjzY.0.p5UIoXHN1R4Go3SjVPs0mx" -d "client_secret=7899378653052916471" -d "username=${username}" -d "password=${password}" -H "X-PrettyPrint:1"`
+access_token=( $(echo ${login} | jq -r '.access_token') )
 
 echo "Access token: ${access_token}"
 
