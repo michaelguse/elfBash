ce1f14883756627737de890b8814031d793a3c25
Michael Guse <mguse@salesforce.com>
2018-05-01 17:08:37 -0400

    Changed accesstoken to sessionid in SOAP login request string

--- bin/load-hourly-elf.sh
+++ bin/load-hourly-elf.sh
@@ -141,8 +141,7 @@ printf 'Event log start TS:\t\t %s\n' ${day}
 echo ===
 echo === [$(date "+%Y-%m-%d %H:%M:%S")] - Retrieve access token from Salesforce sandbox environment 
 echo ===
-access_token=`curl --trace curltrace.txt https://login.salesforce.com/services/Soap/u/v${api_version} -H "Content-Type: text/xml; charset=UTF-8" -H "SOAPAction: login" -d @$HOME/bin/sfdcprod.login | jq -r '.access_token'`
-less curltrace.txt
+access_token=`curl -v https://login.salesforce.com/services/Soap/u/v${api_version} -H "Content-Type: text/xml; charset=UTF-8" -H "SOAPAction: login" -d @$HOME/bin/sfdcprod.login | jq -r '.sessionId'`
 
 #login=`curl -v https://${instance}.salesforce.com/services/oauth2/token -d "grant_type=password" -d "client_id=3MVG99OxTyEMCQ3ilfR5dFvVjgTrCbM3xX8HCLLS4GN72CCY6q86tRzvtjzY.0.p5UIoXHN1R4Go3SjVPs0mx" -d "client_secret=7899378653052916471" -d "username=${username}" -d "password=${password}" -H "X-PrettyPrint:1"`
 #access_token=( $(echo ${login} | jq -r '.access_token') )
